acafff4a01a96b7b7f19b647020b53b7cd474302
Christopher Ivy <Chrisivy22703@gmail.com>
2023-09-12 15:12:40 -0500

    Created the Login and Registration code, tested login/reg/logout, all work fine in postman. also created functions for finding all users, finding one user, editing a user with pregilled information, and deleting a user. updated the user model to include Bcrypt and having it hash the users password, and added a JWT config for future authentication of routes, also added the .env file for the secret key and lastly I updated the server.js file to include everything.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
+      required: [true, "Username is required"],
     },
     password: {
       type: String,
+      required: [true, "Password is required"],
     },
     colors: {
       type: Array,
@@ -17,5 +20,29 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//* MiddleWare
+UserSchema.virtual("confirmPassword")
+  .get(() => this.confirmPassword)
+  .set((value) => (this.confirmPassword = value));
+
+//* Compare Passwords
+UserSchema.pre("validate", function (next) {
+  // console.log("Made it to the validate function")
+  if (this.password !== this.confirmPassword) {
+    // console.log("Passwords do not match");
+    this.invalidate("confirmPassword", "Passwords must match");
+  }
+  next();
+});
+
+//* Hashing Password
+UserSchema.pre("save", function (next) {
+  bcrypt.hash(this.password, 10).then((hash) => {
+    this.password = hash;
+    console.log(hash)
+    next();
+  });
+});
 
 module.exports = mongoose.model("User", UserSchema);
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,17 @@
 const UserController = require("../controllers/user.controller");
 
 module.exports = (app) => {
-  app.get("/api/test", UserController.testMethod);
+  // app.get("/api/test", UserController.testMethod);
+  //* Register/Login/Logout a user
+  app.post("/api/register", UserController.registerUser);
+  app.post("/api/login", UserController.loginUser);
+  app.post("/api/logout", UserController.logoutUser);
+  //* Find all users
+  app.get("/api/users", UserController.findAllUsers);
+  //* Find one user
+  app.get("/api/users/:id", UserController.findOneUser);
+  //* Update a user
+  app.patch("/api/users/:id", UserController.updateUser);
+  //* Delete a user
+  app.delete("/api/users/:id", UserController.deleteUser);
 };
