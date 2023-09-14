dafdc01d8d6c7c9d55c05adb6777ddc8654556f2
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-09-14 18:28:43 -0400

    add logout functionality

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,24 @@
-const Home = ({ userData }) => {
-  console.log(userData);
-  return <h1>Welcome {userData.username}!</h1>;
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = ({ userData, setUserData }) => {
+  const logoutHandler = async () => {
+    await axios
+      .post("http://localhost:8000/api/logout", {}, { withCredentials: true })
+      .then(() => {
+        sessionStorage.clear();
+        setUserData({});
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <h1>Welcome {userData.id ? userData.username : "Guest"}!</h1>
+      {userData.id && <button onClick={logoutHandler}>Logout</button>}
+      {!userData.id && <Link to={"/login"}>Login</Link>}
+    </>
+  );
 };
 
 export default Home;
