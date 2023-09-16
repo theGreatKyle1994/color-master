8126a6ef0df570b5cdf53ed38aad38bca4fe25c2
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-09-16 14:48:34 -0400

    create authentication state variable

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,28 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { globalContext } from "../App";
 
-const Home = ({ userData, setUserData }) => {
+const Home = () => {
+  const { userData, setUserData, isAuthenticated, setIsAuthenticated } =
+    useContext(globalContext);
+
   const logoutHandler = async () => {
     await axios
       .post("http://localhost:8000/api/logout", {}, { withCredentials: true })
       .then(() => {
         sessionStorage.clear();
         setUserData({});
+        setIsAuthenticated(false);
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <>
-      <h1>Welcome {userData.id ? userData.username : "Guest"}!</h1>
-      {userData.id && <button onClick={logoutHandler}>Logout</button>}
-      {!userData.id && <Link to={"/login"}>Login</Link>}
+      <h1>Welcome {isAuthenticated ? userData.username : "Guest"}!</h1>
+      {isAuthenticated && <button onClick={logoutHandler}>Logout</button>}
+      {!isAuthenticated && <Link to={"/login"}>Login</Link>}
     </>
   );
 };
