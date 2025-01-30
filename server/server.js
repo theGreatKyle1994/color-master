548521c26f2e7f97410bc5393061c2edd14b1169
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-01-30 17:31:05 -0500

    update client host/port .env support

--- server/server.js
+++ server/server.js
@@ -4,15 +4,20 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const port = 8000;
 
-require('dotenv').config();
+require("dotenv").config();
 require("./config/mongoose.config");
 require("./config/jwt.config");
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: `${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
+  })
+);
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-//! routes has to be after everything else 
+//! routes has to be after everything else
 //! if this is moved the app will break
 //! and Chris will be sad
 const UserRoutes = require("./routes/user.routes");
@@ -21,6 +26,5 @@ const PaletteRoutes = require("./routes/palette.routes");
 UserRoutes(app);
 ColorRoutes(app);
 PaletteRoutes(app);
-
 
 app.listen(port, () => console.log(`Server live on port: ${port}`));
