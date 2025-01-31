29f5dfb44da6ee15e80b3af9cc79625ba07a38fa
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-01-30 21:49:11 -0500

    update server port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 require("dotenv").config();
 require("./config/mongoose.config");
