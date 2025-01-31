dcc7b5263c265c02ba8d51f17e50fc5415de0b1c
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-01-30 20:52:39 -0500

    update cors policy

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: `${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
+    origin: `http://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
   })
 );
 app.use(express.json(), express.urlencoded({ extended: true }));
