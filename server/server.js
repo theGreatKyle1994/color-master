638b0c687b9d427429fa4c7e631d32fc9ec59b50
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-02-01 16:18:59 -0500

    add cors origin for prod

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: `${process.env.CLIENT_HOST}`,
+    origin: true,
   })
 );
 app.use(express.json(), express.urlencoded({ extended: true }));
